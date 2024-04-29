18c4555f63b373848ab54cc938fe168cbb199a53
shanyouli <shanyouli@users.noreply.github.com>
2024-04-29 16:32:09 +0000

    auto: update package hashes

--- pkgs/common/mihomo.nix
+++ pkgs/common/mihomo.nix
@@ -5,7 +5,7 @@
 }:
 buildGoModule rec {
   inherit (source) pname src;
-  vendorHash = "sha256-k4xB/jO78VGD+n9HtuoWXoXB+kZCEyPKJTTwj32nGIw=";
+  vendorHash = "sha256-9Ey4roUm/Y6GfFmLD6ij/A+YIgetStawWmhD+Iq3qc8=";
   version =
     if (builtins.hasAttr "date" source)
     then source.date
