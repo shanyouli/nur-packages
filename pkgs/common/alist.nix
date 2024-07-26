cd997f7f29a9f5c6085e2b9ade5eb905f4fcb737
shanyouli <shanyouli@users.noreply.github.com>
2024-07-26 22:48:43 +0000

    auto: update package hashes

--- pkgs/common/alist.nix
+++ pkgs/common/alist.nix
@@ -19,7 +19,7 @@ in
     inherit (source) pname src;
     # FIXME:  import lookup disabled by -mod=vendor
     # see @https://github.com/NixOS/nixpkgs/issues/273998
-    vendorHash = "sha256-XaccbVgvt7qsuacbxqgonOkiRBiRa11ITnyrefvJvwU=";
+    vendorHash = "sha256-fta1fBKX6/vMetta4XLtk68BXyr4T6Gnb/WpHlo2P9E=";
     proxyVendor = true;
     preBuild = ''
       rm -rf public/dist
