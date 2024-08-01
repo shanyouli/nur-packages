8ad55142b543af48f4a00e819f7e324af6baff9a
Shanyou Li <shanyouli6@gmail.com>
2024-08-01 19:48:24 +0800

    feat: add tmux-fastcopy

---
+++ pkgs/common/tmux-fastcopy.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  buildGoModule,
+  source,
+}:
+buildGoModule rec {
+  inherit (source) pname src;
+  vendorHash = "sha256-Jcx9/qJKR4q1EYUu6NsNkakJS/qtQLlhys0GKx5BLQk=";
+  subPackages = ["."];
+  version =
+    if (builtins.hasAttr "date" source)
+    then source.date
+    else lib.removePrefix "v" source.version;
+
+  ldflags = ["-s" "-w"];
+  # postInstall = ''
+  #   mv $out/bin/DeepLX $out
+  #   mv $out/DeepLX $out/bin/deeplx
+  # '';
+  checkFlags = let
+    skippedTests = [
+      "TestArgAction_RegexesEnv"
+    ];
+  in ["--skip=^${builtins.concatStringsSep "$|^" skippedTests}$"];
+  meta = with lib; {
+    description = "easymotion-style text copying for tmux.";
+    homepage = "https://github.com/abhinav/tmux-fastcopy/tree/main";
+    license = licenses.gpl2;
+    mainProgram = "tmux-fastcopy";
+  };
+}
