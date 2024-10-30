5328976e2061ff44ceb5a76b1dd56862d84a90fd
Shanyou Li <shanyouli6@gmail.com>
2024-10-30 11:33:54 +0800

    add: treefmt-nix

---
+++ nix/dev/treefmt.nix
@@ -0,0 +1,42 @@
+{inputs, ...}: {
+  imports = [inputs.treefmt-nix.flakeModule];
+  perSystem = {config, ...}: {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      settings.global.excludes = [
+        "hosts/**/hardware-*.nix"
+        "*.gpg"
+        "*.lock"
+        "orbstack.nix"
+        "generated.nix"
+        "bbdown/deps.nix"
+      ];
+
+      programs = {
+        # nix format
+        statix.enable = true; # nix static analysis
+        deadnix.enable = true; # find dead nix code
+        alejandra.enable = true; # nix fmt tools
+
+        # python
+        ruff.enable = true;
+        ruff.check = true;
+        # lua
+        # stylua.enable = true;
+
+        # # bash
+        # shellcheck.enable = false;
+        # shfmt = {
+        #   enable = true;
+        #   indent_size = 4;
+        # };
+
+        # # json
+        # jsonfmt.enable = true;
+        # # justfile
+        # just.enable = true;
+      };
+    };
+    formatter = config.treefmt.build.wrapper;
+  };
+}
