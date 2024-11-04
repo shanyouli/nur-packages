95e34333c9a5274a1f88ded200a59b0335dd47c4
Shanyou Li <shanyouli6@gmail.com>
2024-11-04 20:53:53 +0800

    dev: singbop

---
+++ pkgs/common/singboxp.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  stdenv,
+  buildGoModule,
+  installShellFiles,
+  buildPackages,
+  coreutils,
+  source,
+}:
+buildGoModule rec {
+  inherit (source) pname src;
+  version =
+    if (builtins.hasAttr "date" source)
+    then source.date
+    else lib.removePrefix "v" source.version;
+
+  vendorHash = "sha256-7GQTsicl420bXUPT09sBn+OLBA5Ppi8OPWJys8mNe+c=";
+
+  tags = [
+    "with_quic"
+    "with_grpc"
+    "with_dhcp"
+    "with_wireguard"
+    "with_ech"
+    "with_utls"
+    "with_reality_server"
+    "with_acme"
+    "with_clash_api"
+    "with_v2ray_api"
+    "with_gvisor"
+  ];
+
+  subPackages = [
+    "cmd/sing-box"
+  ];
+
+  nativeBuildInputs = [installShellFiles];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=github.com/sagernet/sing-box/constant.Version=${version}"
+  ];
+
+  postInstall = let
+    emulator = stdenv.hostPlatform.emulator buildPackages;
+  in ''
+    installShellCompletion --cmd sing-box \
+      --bash <(${emulator} $out/bin/sing-box completion bash) \
+      --fish <(${emulator} $out/bin/sing-box completion fish) \
+      --zsh  <(${emulator} $out/bin/sing-box completion zsh )
+
+    substituteInPlace release/config/sing-box{,@}.service \
+      --replace-fail "/usr/bin/sing-box" "$out/bin/sing-box" \
+      --replace-fail "/bin/kill" "${coreutils}/bin/kill"
+    install -Dm444 -t "$out/lib/systemd/system/" release/config/sing-box{,@}.service
+  '';
+
+  meta = with lib; {
+    homepage = "https://sing-box.sagernet.org";
+    description = "The universal proxy platform";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [nickcao];
+    mainProgram = "sing-box";
+  };
+}
