cdf73d374e2fb725b3ede7ed71e7fb3204cb6fe3
Shanyou Li <shanyouli6@gmail.com>
2024-11-19 16:20:26 +0800

    fix: dutis error

--- pkgs/darwin/dutis.nix
+++ pkgs/darwin/dutis.nix
@@ -4,28 +4,34 @@
   makeWrapper,
   duti,
   source,
-}:
-buildGoModule rec {
-  inherit (source) pname src;
-  vendorHash = "sha256-FD9WC754G/t8BJEyNBzokZePSyduR/ELnG3Dh7boGWs=";
-  version =
-    if (builtins.hasAttr "date" source)
-    then source.date
-    else lib.removePrefix "v" source.version;
+  apple-sdk,
+  ...
+}: let
+  newduti = duti.overrideAttrs (old: {
+    buildInputs = (old.buildInputs or []) ++ [apple-sdk];
+    configureFlags = [
+      "--with-macosx-sdk=${apple-sdk.sdkroot}"
+      "--host=x86_64-apple-darwin18"
+    ];
+  });
+in
+  buildGoModule rec {
+    inherit (source) pname src version;
+    vendorHash = "sha256-FD9WC754G/t8BJEyNBzokZePSyduR/ELnG3Dh7boGWs=";
 
-  nativeBuildInputs = [makeWrapper];
+    nativeBuildInputs = [makeWrapper];
 
-  ldflags = ["-s" "-w"];
-  doCheck = false;
-  postInstall = ''
-    wrapProgram $out/bin/dutis \
-      --prefix PATH : ${lib.makeBinPath [duti]}
-  '';
-  meta = with lib; {
-    description = "A command-line tool to select default applications, based on duti";
-    homepage = "https://github.com/tsonglew/dutis";
-    license = licenses.mit;
-    platforms = platforms.darwin;
-    mainProgram = "dutis";
-  };
-}
+    ldflags = ["-s" "-w"];
+    doCheck = false;
+    postInstall = ''
+      wrapProgram $out/bin/dutis \
+        --prefix PATH : ${lib.makeBinPath [newduti]}
+    '';
+    meta = with lib; {
+      description = "A command-line tool to select default applications, based on duti";
+      homepage = "https://github.com/tsonglew/dutis";
+      license = licenses.mit;
+      platforms = platforms.darwin;
+      mainProgram = "dutis";
+    };
+  }
