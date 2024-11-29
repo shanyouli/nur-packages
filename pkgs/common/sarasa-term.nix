61ea76e92093e02968fd94a849df6f81bdd66dc5
Shanyou Li <shanyouli6@gmail.com>
2024-11-29 17:37:51 +0800

    feat: remove mpvc

---
+++ pkgs/common/sarasa-term.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  stdenvNoCC,
+  source,
+}:
+stdenvNoCC.mkDerivation {
+  inherit (source) src pname;
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/fonts/ttc
+    find -name \*.ttc -exec mv {} $out/share/fonts/ttc \;
+    runHook postInstall
+  '';
+  meta = with lib; {
+    description = "中英文宽度完美 2:1 字体";
+    homepage = "https://github.com/laishulu/Sarasa-Term-SC-Nerd";
+    platforms = platforms.all;
+  };
+}
