577295cd10c2ea0a2c228d4ddf77fea4078a2f8b
Shanyou Li <shanyouli6@gmail.com>
2024-12-09 16:09:00 +0800

    fix: bbdown error

--- pkgs/common/bbdown/default.nix
+++ pkgs/common/bbdown/default.nix
@@ -31,29 +31,22 @@ buildDotnetModule rec {
       darwin.ICU
       xcbuild
     ];
-  # 仅在macos上测试
-  # preConfigure = lib.optionalString stdenv.hostPlatform.isDarwin ''
-  #     export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=0
-  #     export LIBRARY_PATH=$LIBRARY_PATH:${darwin.apple_sdk.MacOSX-SDK}/usr/lib/:${darwin.apple_sdk.MacOSX-SDK}/usr/lib/swift/
-  #   '';
-  # BUGS: see @  https://github.com/NixOS/nixpkgs/issues/280923
-  # + lib.optionalString stdenv.hostPlatform.isLinux ''
-  #   TOOLS="$HOME/.nuget/packages/microsoft.netcore.app.crossgen2.linux-x64/${dotnetCorePackages.runtime_8_0.version}/tools/"
-  #   TOOLS="$TOOLS:$HOME/.nuget/packages/runtime.linux-x64.microsoft.dotnet.ilcompiler/${dotnetCorePackages.runtime_8_0.version}/tools/"
-  #   export LD_LIBRARY_PATH="$LDLIBRARY_PATH:$TOOLS"
-  # '';
   preBuild = ''
     export projectFile=(BBDown)
   '';
-  dotnetFlags = ["-p:PublishTrimmed=true"] ++ lib.optionals stdenv.hostPlatform.isDarwin ["-p:StripSymbols=false"];
+  # dotnetFlags = ["-p:PublishTrimmed=true"] ++ lib.optionals stdenv.hostPlatform.isDarwin ["-p:StripSymbols=false"];
+  dotnetFlags = [
+    "-p:PublishReadyToRun=false"
+    # for some reason this is set to win-x64 in the project files
+    "-p:RuntimeIdentifier="
+  ];
   dotnetInstallFlags = ["--framework=net9.0"];
   selfContainedBuild = true;
   dotnet-runtime = dotnetCorePackages.runtime_9_0;
   runtimeDeps = [];
-  postFixup = ''
-    ${lib.optionalString stdenv.hostPlatform.isDarwin ''/usr/bin/strip $out/lib/BBDown/BBDown''}
-    ln -s ~/.config/BBDown/BBDown.data $out/lib/BBDown/BBDown.data || true
-  '';
+  # postFixup = ''
+  #   ${lib.optionalString stdenv.hostPlatform.isDarwin ''/usr/bin/strip $out/lib/BBDown/BBDown''}
+  # '';
   meta = with lib; {
     homepage = "https://github.com/nilaoda/BBDown";
     description = "Bilibili Downloader. 一款命令行式哔哩哔哩下载器.";
