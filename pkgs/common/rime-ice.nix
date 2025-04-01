a8fc27be99eab1b56f1c612db3983e593487c562
Shanyou Li <shanyouli6@gmail.com>
2025-04-01 18:59:48 +0800

    feat(pkg): add rime

---
+++ pkgs/common/rime-ice.nix
@@ -0,0 +1,27 @@
+{
+  source,
+  stdenvNoCC,
+  lib,
+}:
+stdenvNoCC.mkDerivation rec {
+  inherit (source) pname src version;
+  buildPhase = ''
+    runHook preBuild
+    mv default.yaml rime_ice_suggestion.yaml
+    runHook postBuild
+  '';
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/rime-data
+    cp -r * $out/share/rime-data/
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Rime 配置:雾凇拼音";
+    homepage = "https://dvel.me/posts/rime-ice/";
+    license = lib.licenses.gpl3Only;
+  };
+}
