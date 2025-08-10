7dbe369c501514b7a8d0aa30997dcbcdea1e3df3
Shanyou Li <shanyouli6@gmail.com>
2025-08-10 22:13:03 +0800

    feat: fix musicn

--- pkgs/common/musicn.nix
+++ pkgs/common/musicn.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm_9.fetchDeps {
     inherit (finalAttrs) pname version src;
-    fetcherVersion = finalAttrs.version;
+    fetcherVersion = 1;
     hash = "sha256-/cxuMTcMKU1jwkvQhPI/+Em1sadBDEJraR54rBw/7wk=";
   };
   nativeBuildInputs = [
