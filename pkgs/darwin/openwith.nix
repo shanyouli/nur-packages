c3c4f740e0e680ca7cb26f6badc423886e419c7d
Shanyou Li <shanyouli6@gmail.com>
2026-04-29 21:59:21 +0800

    feat: add openwith

---
+++ pkgs/darwin/openwith.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  rustPlatform,
+  source,
+  apple-sdk,
+  ...
+}:
+rustPlatform.buildRustPackage rec {
+  inherit (source) pname src version;
+  cargoLock = source.cargoLock."Cargo.lock";
+  doCheck = false;
+  buildInputs = [ apple-sdk ];
+  meta = with lib; {
+    description = "Manage macOS file extension associations from the terminal ";
+    homepage = "https://github.com/ColeMei/openwith/tree/main";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+    mainProgram = "openwith";
+  };
+}
