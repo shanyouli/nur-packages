cc899a5f4e4a958c825ca5b475e0d9f0bef93291
Shanyou Li <shanyouli6@gmail.com>
2026-05-19 18:09:42 +0800

    feat: adjusc test

--- pkgs/common/jcode.nix
+++ pkgs/common/jcode.nix
@@ -2,10 +2,14 @@
   rustPlatform,
   source,
   lib,
+  pkg-config,
 }:
 rustPlatform.buildRustPackage rec {
   inherit (source) pname version src;
-  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  nativeBuildInputs = [
+    rustPlatform.bindgenHook
+    pkg-config
+  ];
   cargoLock = source.cargoLock."Cargo.lock";
   meta = with lib; {
     description = "Coding Agent Harness";
