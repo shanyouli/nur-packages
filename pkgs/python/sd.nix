e973d0641e8380d2d7efae4116feb3b7305c1f7d
Shanyou Li <shanyouli6@gmail.com>
2026-06-01 14:38:36 +0800

    dev: fix sd build error

--- pkgs/python/sd.nix
+++ pkgs/python/sd.nix
@@ -14,15 +14,19 @@ buildPythonApplication rec {
     if (builtins.hasAttr "date" source) then source.date else lib.removePrefix "v" source.version;
   inherit (source) pname src;
   pyproject = true;
+
+  build-system = [ uv-build ];
+
   nativeBuildInputs = [
-    uv-build
     installShellFiles
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [ darwin.ps ];
+
   propagatedBuildInputs = [
     typer
     wcwidth
   ];
+
   postInstall = ''
     installShellCompletion --cmd sd \
       --bash <($out/bin/sd completion show bash) \
