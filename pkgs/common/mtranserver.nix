26c94f9abb841229526d3164d35b1ff585e6f287
Shanyou Li <shanyouli6@gmail.com>
2026-07-09 09:19:41 +0800

    chore: switch MTranServer to source build and update dev config

--- pkgs/common/mtranserver.nix
+++ pkgs/common/mtranserver.nix
@@ -1,41 +1,115 @@
-# 编译太复杂，直接使用 预编译
 {
   lib,
-  stdenvNoCC,
-  sources,
+  stdenv,
+  fetchFromGitHub,
+  bun,
+  writableTmpDirAsHomeHook,
 }:
-let
-  source =
-    if stdenvNoCC.isLinux then
-      if stdenvNoCC.isAarch64 then
-        sources."mtranserver.linux.arm64"
-      else
-        sources."mtranserver.linux.amd64"
-    else if stdenvNoCC.isAarch64 then
-      sources."mtranserver.mac.arm64"
-    else
-      sources."mtranserver.mac.amd64";
+
+stdenv.mkDerivation (finalAttrs: {
   pname = "mtranserver";
-in
-stdenvNoCC.mkDerivation {
-  inherit (source) src version;
-  inherit pname;
-  phases = [ "installPhase" ];
+  version = "4.0.33";
+
+  src = fetchFromGitHub {
+    owner = "xxnuo";
+    repo = "MTranServer";
+    rev = "v${finalAttrs.version}";
+    hash = lib.fakeHash;
+  };
+
+  bunDeps = stdenv.mkDerivation {
+    pname = "${finalAttrs.pname}-bun-deps";
+    inherit (finalAttrs) version src;
+
+    nativeBuildInputs = [
+      bun
+      writableTmpDirAsHomeHook
+    ];
+
+    dontConfigure = true;
+
+    buildPhase = ''
+      runHook preBuild
+
+      export BUN_INSTALL_CACHE_DIR=$(mktemp -d)
+      bun install \
+        --frozen-lockfile \
+        --ignore-scripts \
+        --no-progress
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -R node_modules $out/node_modules
+
+      runHook postInstall
+    '';
+
+    dontFixup = true;
+    outputHash = lib.fakeHash;
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  };
+
+  nativeBuildInputs = [
+    bun
+    writableTmpDirAsHomeHook
+  ];
+
+  configurePhase = ''
+    runHook preConfigure
+
+    cp -R ${finalAttrs.bunDeps}/node_modules .
+    patchShebangs node_modules
+
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    bun run build:all
+
+    runHook postBuild
+  '';
+
   installPhase = ''
     runHook preInstall
-    install -D -m755 $src $out/bin/mtranserver
+
+    mkdir -p $out/bin
+    mtranserver_bin="$(
+      find . -type f \( -name mtranserver -o -name MTranServer \) -perm -0100 \
+        | head -n 1
+    )"
+
+    if [ -z "$mtranserver_bin" ]; then
+      echo "mtranserver binary was not found; update installPhase after checking build:all output" >&2
+      exit 1
+    fi
+
+    install -Dm755 "$mtranserver_bin" "$out/bin/mtranserver"
+
     runHook postInstall
   '';
+
+  doCheck = false;
+
+  passthru = { inherit (finalAttrs) bunDeps; };
 
   meta = with lib; {
     description = "Offline translation model server";
     homepage = "https://github.com/xxnuo/MTranServer";
+    sourceProvenance = with sourceTypes; [ fromSource ];
     platforms = [
       "x86_64-linux"
       "aarch64-linux"
       "aarch64-darwin"
-      "x86_64=darwin"
+      "x86_64-darwin"
     ];
     mainProgram = "mtranserver";
   };
-}
+})
